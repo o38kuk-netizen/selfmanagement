a3f9d2c6bd3fa0c38867c2223da6307505ecff27
Koki Oshiro <o38kuk@gmail.com>
2026-07-08 23:40:42 +0900

    Add complete React app structure with package.json and all necessary files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import HabitTracker from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <HabitTracker />
+  </React.StrictMode>
+);
